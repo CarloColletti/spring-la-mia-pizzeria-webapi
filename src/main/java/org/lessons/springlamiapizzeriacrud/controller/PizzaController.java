61274f072b737fa4c9614c72177d445c96d105f6
CarloColletti~ <carlocolletti5@gmail.com>
2023-06-22 17:44:54 +0200

    preparata la rotta

--- src/main/java/org/lessons/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/controller/PizzaController.java
@@ -1,8 +1,16 @@
 package org.lessons.springlamiapizzeriacrud.controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.List;
-
+@Controller
+@RequestMapping("/")
 public class PizzaController {
-
+    @GetMapping
+    public String index() {
+        return "index";
+    }
 }
