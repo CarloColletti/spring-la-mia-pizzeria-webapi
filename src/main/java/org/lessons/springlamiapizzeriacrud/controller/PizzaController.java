d4ec2cb9067eb27dde6cfccb4fe8edf9cd8aa824
CarloColletti~ <carlocolletti5@gmail.com>
2023-06-26 17:42:45 +0200

    pizza salvata senza validazioni

--- src/main/java/org/lessons/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/controller/PizzaController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.awt.print.Book;
@@ -59,7 +56,9 @@ public class PizzaController {
 
     // controller che gestisce il post del form
     @PostMapping("/create")
-    public String store(){
+    public String store(@ModelAttribute("pizza") Pizza formPizza
+    ) {
+        pizzaRepository.save(formPizza);
         return "redirect:/pizza";
     }
 }
