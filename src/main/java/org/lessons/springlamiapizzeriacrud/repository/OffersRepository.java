d8e0be9db99e4c518fd92901a1ff45a7a333943e
CarloColletti~ <carlocolletti5@gmail.com>
2023-06-29 16:41:53 +0200

    add OffersRepository

---
+++ src/main/java/org/lessons/springlamiapizzeriacrud/repository/OffersRepository.java
@@ -0,0 +1,7 @@
+package org.lessons.springlamiapizzeriacrud.repository;
+
+import org.lessons.springlamiapizzeriacrud.model.Offers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OffersRepository extends JpaRepository<Offers, Integer> {
+}
