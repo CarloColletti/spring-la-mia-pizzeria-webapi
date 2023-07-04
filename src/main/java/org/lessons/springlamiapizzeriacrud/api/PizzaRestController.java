8fd0f51f8d7ad92a46bcfd98bf0beadcf65caa5d
CarloColletti~ <carlocolletti5@gmail.com>
2023-07-04 17:20:10 +0200

    creata show

--- src/main/java/org/lessons/springlamiapizzeriacrud/api/PizzaRestController.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/api/PizzaRestController.java
@@ -3,12 +3,13 @@ package org.lessons.springlamiapizzeriacrud.api;
 import org.lessons.springlamiapizzeriacrud.model.Pizza;
 import org.lessons.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 //controller risorsa pizza
 @RestController
@@ -20,9 +21,22 @@ public class PizzaRestController {
     private PizzaRepository pizzaRepository;
 
     //servizio per la lista delle pizze
+
+    //index_________________________________
     @GetMapping
     public List<Pizza> index() {
         return pizzaRepository.findAll();
+    }
+
+    //show_________________________________
+    @GetMapping("/{id}")
+    public Pizza show( @PathVariable Integer id ) {
+        Optional<Pizza> pizza = pizzaRepository.findById(id);
+        if(pizza.isPresent()){
+            return pizza.get();
+        } else {
+          throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 
 
